6980393f44bdbffa80b5c1dca4a7b87fa13d6994
dissdi <iksdguri@gmail.com>
2026-01-15 17:29:46 +0900

    update preprocessor

--- dprdataset/nqdataset.py
+++ dprdataset/nqdataset.py
@@ -1,104 +1,37 @@
-from datasets import load_dataset, Features, Value, Sequence
-from transformers import AutoTokenizer, DataCollatorWithPadding
+import torch
 import random
+from datasets import load_from_disk
+from torch.utils.data import Dataset
 
-from torch.utils.data import DataLoader
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+def _batch_to_tensor(xs: list, dtype=torch.long):
+    if torch.is_tensor(xs[0]):
+        return torch.stack(xs)
 
-def load_w100_dataset():
-    return load_dataset(
-        'csv',
-        data_files='downloads/data/wikipedia_split/psgs_w100.tsv',
-        sep='\t',
-        streaming=True
-    )
-
-# dataset 구조
-# [
-#   {
-# 	"question": "....",
-# 	"answers": ["...", "...", "..."],
-# 	"positive_ctxs": [{
-# 		"title": "...",
-# 		"text": "...."
-# 	}],
-# 	"negative_ctxs": ["..."],
-# 	"hard_negative_ctxs": ["..."]
-#   },
-#   ...
-# ]
-
-def load_nq_dataset(path):
-    ctx = Features({
-    "passage_id": Value("string"),
-    "score": Value("float64"),       # score는 전부 float로 통일 권장 (int도 float로 캐스팅 가능)
-    "text": Value("large_string"),
-    "title": Value("string"),
-    "title_score": Value("int64"),
-    })
-
-    features = Features({
-        "dataset": Value("string"),
-        "question": Value("string"),
-        "answers": Sequence(Value("string")),
-        "positive_ctxs": Sequence(ctx),
-        "negative_ctxs": Sequence(ctx),
-        "hard_negative_ctxs": Sequence(ctx),
-    })
-
-    return load_dataset(
-        'json', 
-        data_files=path, 
-        split='train',
-        features=features
-    )
+    return torch.tensor(xs, dtype=dtype)
 
 def collate_fn(batch):
-    questions = [item["question"] for item in batch]
+    q_input_ids= _batch_to_tensor([item["query"]["input_ids"] for item in batch])
+    q_attention_mask = _batch_to_tensor([item["query"]["attention_mask"] for item in batch])
 
-    positives = []
-    for item in batch:
-        selected_p = random.choice(item["positive_ctxs"])
-        formatted_p = f"{selected_p['title']}{tokenizer.sep_token}{selected_p['text']}"
-        positives.append(formatted_p)
+    chosen = [random.choice(item["positive_passages"]) for item in batch]
 
-    hard_negatives = []
-    for item in batch:
-        pool = item["hard_negative_ctxs"] if item["hard_negative_ctxs"] else item["negative_ctxs"]
-        selected_hn = random.choice(pool)
-        
-        formatted_hn = f"{selected_hn['title']}{tokenizer.sep_token}{selected_hn['text']}"
-        hard_negatives.append(formatted_hn)
+    p_input_ids = _batch_to_tensor([item["token"]["input_ids"] for item in chosen])
+    p_attention_mask = _batch_to_tensor([item["token"]["attention_mask"] for item in chosen])
 
-    q_inputs = tokenizer(
-        questions,
-        padding=True,
-        truncation=True,
-        return_tensors="pt"
-    )
+    chosen = [random.choice(item["negative_passages"]) for item in batch]
 
-    p_inputs = tokenizer(
-        positives,
-        padding=True,
-        truncation=True,
-        return_tensors="pt"
-    )
-    
-    hn_inputs = tokenizer(
-        hard_negatives,
-        padding=True,
-        truncation=True,
-        return_tensors="pt"
-    )
+    hn_input_ids = _batch_to_tensor([item["token"]["input_ids"] for item in chosen])
+    hn_attention_mask = _batch_to_tensor([item["token"]["attention_mask"] for item in chosen])
 
     return {
-        "q_input_ids": q_inputs['input_ids'],
-        "q_attention_mask": q_inputs['attention_mask'],
-        
-        "p_input_ids": p_inputs['input_ids'],
-        "p_attention_mask": p_inputs['attention_mask'], 
-        
-        "hn_input_ids": hn_inputs['input_ids'],        
-        "hn_attention_mask": hn_inputs['attention_mask']
+        "q_input_ids": q_input_ids,
+        "q_attention_mask": q_attention_mask,
+        "p_input_ids": p_input_ids,
+        "p_attention_mask": p_attention_mask,
+        "hn_input_ids": hn_input_ids,
+        "hn_attention_mask": hn_attention_mask,
     }
+
+def load_nq_dataset(file_path: str) -> Dataset:
+    return load_from_disk(file_path)
