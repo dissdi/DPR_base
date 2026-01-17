0593ca2115926eb268154817b9394fce8acaf7e4
Talusor <lth030326@naver.com>
2026-01-17 19:27:21 +0900

    benchmark code

---
+++ faiss_benchmark.py
@@ -0,0 +1,64 @@
+import numpy as np
+import torch
+import tqdm
+import faiss
+from safetensors.torch import load_model
+
+from dprdataset.nqdataset import load_nq_dataset, collate_fn
+from torch.utils.data import DataLoader
+
+from models import DPR
+
+
+def benchmark_recall_k(model, index, dataset_path, k = 1, batch_size = 256):
+    valid_dataset = load_nq_dataset(dataset_path)
+    dataloader = DataLoader(valid_dataset, collate_fn=collate_fn, batch_size=batch_size, shuffle=False, num_workers=6,
+                            pin_memory=True,
+                            prefetch_factor=6, persistent_workers=True)
+    model.eval()
+    with torch.no_grad():
+        recall = 0
+        N = 0
+        for step, batch in enumerate(tqdm.tqdm(dataloader, desc=f"Benchmark Recall@{k}", unit="batch")):
+            q_ids = batch["q_input_ids"].to("cuda")
+            q_mask = batch["q_attention_mask"].to("cuda")
+            q_token_ids = batch["q_token_type_ids"].to("cuda")
+
+            query = model.encode_query(q_ids, q_mask, q_token_ids).cpu().numpy().astype(np.float32)
+
+            dist, indices = index.search(query, k)
+
+            labels = batch["passage_ids"]
+
+            for result, label in zip(indices, labels):
+                N += 1
+                if set(result) & set(label):
+                    recall += 1
+
+        return recall / N
+
+
+if __name__ == "__main__":
+    FAISS_INDEX_PATH = "./output/faiss.index"
+    MODEL_PATH = "./output/~~/model.safetensors"
+    DATASET_PATH = "./data/raw_data/nq-dev"
+    BATCH_SIZE = 256
+    Ks = [5, 20, 50, 100]
+    NPROBE = 64 # IVF parameter
+
+    print("Benchmark Recall@K")
+
+    index = faiss.read_index(FAISS_INDEX_PATH)
+    index.nprobe = NPROBE
+
+    model = DPR()
+    load_model(model, MODEL_PATH)
+    model.to("cuda")
+
+    results = []
+
+    for k in Ks:
+        results.append(benchmark_recall_k(model, index, DATASET_PATH, k = k, batch_size = BATCH_SIZE))
+
+    for k, recall in zip(Ks, results):
+        print(f"Recall@{k}: {recall:.3f}", flush=True)
