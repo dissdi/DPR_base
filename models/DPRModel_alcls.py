c3f48da8913b873bc48b73bf6b80f9e7783e218b
Talusor <lth030326@naver.com>
2026-01-19 06:53:00 +0900

    Add layer-wise CLS attention model

--- models/DPRModel_alcls.py
+++ models/DPRModel_alcls.py
@@ -6,22 +6,34 @@ from transformers import AutoModel
 class DPR_alcls(nn.Module):
     def __init__(self, loss_fn=None):
         super().__init__()
-        self.q_encoder = AutoModel.from_pretrained("bert-base-uncased")
-        self.p_encoder = AutoModel.from_pretrained("bert-base-uncased")
+        self.q_encoder = AutoModel.from_pretrained("bert-base-uncased", output_hidden_states=True)
+        self.p_encoder = AutoModel.from_pretrained("bert-base-uncased", output_hidden_states=True)
+
+        self.layer_attn = LayerAttention(hidden_size=768,
+                                         num_heads=8,
+                                         ffn_mult=4,
+                                         dropout=0.1)
 
         self.loss_fn = loss_fn if loss_fn is not None else nn.CrossEntropyLoss()
 
     def encode_query(self, input_ids=None, attention_mask=None, token_type_ids=None):
-        return self.q_encoder(input_ids=input_ids,
-                              attention_mask=attention_mask,
-                              token_type_ids=token_type_ids,
-                              return_dict=True).last_hidden_state[:, 0, :]
+        layers = self.q_encoder(input_ids=input_ids,
+                                attention_mask=attention_mask,
+                                token_type_ids=token_type_ids,
+                                return_dict=True).hidden_states[1:13]  # bert-base is 12 layer
+
+        cls_layers = [h[:, 0, :] for h in layers]
+        return self.layer_attn(cls_layers)
+
 
     def encode_passage(self, input_ids=None, attention_mask=None, token_type_ids=None):
-        return self.p_encoder(input_ids=input_ids,
-                              attention_mask=attention_mask,
-                              token_type_ids=token_type_ids,
-                              return_dict=True).last_hidden_state[:, 0, :]
+        layers = self.p_encoder(input_ids=input_ids,
+                                attention_mask=attention_mask,
+                                token_type_ids=token_type_ids,
+                                return_dict=True).hidden_states[1:13]  # bert-base is 12 layer
+
+        cls_layers = [h[:, 0, :] for h in layers]
+        return self.layer_attn(cls_layers)
 
     def forward(self,
                 q_input_ids=None, q_attention_mask=None, q_token_type_ids=None,
@@ -50,3 +62,46 @@ class DPR_alcls(nn.Module):
             "loss": loss,
             "logits": sim_matrix
         }
+
+
+#
+class LayerAttention(nn.Module):
+    def __init__(self, hidden_size: int, num_heads: int, ffn_mult: int, dropout: float):
+        super().__init__()
+        self.H = hidden_size
+
+        self.attn = nn.MultiheadAttention(
+            embed_dim=hidden_size,
+            num_heads=num_heads,
+            dropout=dropout,
+            batch_first=True)
+
+        self.ffn = nn.Sequential(
+            nn.Linear(hidden_size, ffn_mult * hidden_size),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(ffn_mult * hidden_size, hidden_size),
+        )
+
+        self.norm1 = nn.LayerNorm(hidden_size)
+        self.norm2 = nn.LayerNorm(hidden_size)
+
+        self.dropout = nn.Dropout(dropout)
+
+        self.info = nn.Parameter(torch.randn(1, 1, hidden_size))
+
+    def forward(self, cls_layers):
+        # cls_layers List[[B, H]]
+        X = torch.stack(cls_layers, dim=1)
+        B, L, H = X.shape
+        info_tok = self.info.expand(B, 1, self.H)
+
+        Z = torch.cat([info_tok, X], dim=1)
+
+        attn_out, _ = self.attn(Z, Z, Z)
+
+        Z = self.norm1(Z + self.dropout(attn_out))
+        ffn_out = self.ffn(Z)
+        Z = self.norm2(Z + self.dropout(ffn_out))
+
+        return Z[:, 0, :]
