0bedc870880a357c9510781427051fea4a753165
dissdi <iksdguri@gmail.com>
2026-01-25 22:07:40 +0900

    tokenizer test code fix

--- models/RiskMCLS_tokenizer.py
+++ models/RiskMCLS_tokenizer.py
@@ -92,7 +92,7 @@ class RiskMCLS_tokenizer(nn.Module):
     
 if __name__ == "__main__":
     # Test code
-    tokenizer = CMCLS_tokenizer()
+    tokenizer = RiskMCLS_tokenizer()
     ta = "big little lies season 2 how many episodes"
     tb = "Does He Love You \"Does He Love You\" is a song written by Sandy Knox and Billy Stritch, and recorded as a duet by American country music artists Reba McEntire and Linda Davis. It was released in August 1993 as the first single from Reba's album \"Greatest Hits Volume Two\". It is one of country music's several songs about a love triangle. \"Does He Love You\" was written in 1982 by Billy Stritch. He recorded it with a trio in which he performed at the time, because he wanted a song that could be sung by the other two members"
 
