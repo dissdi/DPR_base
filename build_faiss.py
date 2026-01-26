20779685495c7ded9405e98f715502f18f1facbb
dissdi <iksdguri@gmail.com>
2026-01-26 16:41:12 +0900

    faiss build error fix

--- build_faiss.py
+++ build_faiss.py
@@ -23,6 +23,8 @@ def build_faiss_index(check_point_dir: Path, BATCH_SIZE=512, STEP=800, PSGS_PATH
 
     model.eval()
 
+    tokenizer = RiskMCLS_tokenizer()
+
     print(f"Start encode passages for train")
 
     qaunt = faiss.IndexFlatIP(768) # Bert CLS Token dim \wo L2 norm (IP = Inner Product)
@@ -32,10 +34,27 @@ def build_faiss_index(check_point_dir: Path, BATCH_SIZE=512, STEP=800, PSGS_PATH
     with torch.no_grad():
         p_embs = []
         for steps, batch in enumerate(tqdm(loader, desc="Encoding passages for train", total=STEP, unit="batch"), start=0):
-            titles = [item["title"] for item in batch]
-            texts = [item["text"] for item in batch]
-            p_token = RiskMCLS_tokenizer()
-            emb = model.encode_passage(**p_token)
+            embs = []
+            # tokenize one by one, as my tokenizer doesnt support batch tokenize
+            for item in batch:
+                p_token = tokenizer.p_tokenize(item["title"], item["text"])
+                
+                # tensor all item
+                p_token = {
+                    "input_ids": torch.tensor(p_token["input_ids"], dtype=torch.long).unsqueeze(0).to("cuda"),
+                    "attention_mask": torch.tensor(p_token["attention_mask"], dtype=torch.long).unsqueeze(0).to("cuda"),
+                    "token_type_ids": torch.tensor(p_token["token_type_ids"], dtype=torch.long).unsqueeze(0).to("cuda"),
+                    "mcls_positions": p_token["mcls_positions"].to("cuda"),
+                    "mcls_mask": p_token["mcls_mask"].to("cuda"),
+                }
+                mcls, mcls_mask = model.encode_passage(**p_token)
+                
+                mcls_mask_ = mcls_mask.unsqueeze(-1).to(mcls.device)
+                denom = mcls_mask_.sum(dim=1).clamp(min=1)          # (1, 1)
+                pooled = (mcls * mcls_mask_).sum(dim=1) / denom     # (1, H)   
+                             
+                embs.append(pooled)
+            emb = torch.cat(embs, dim=0)
             p_embs.append(emb.cpu())
             if steps + 1 >= STEP:
                 break
@@ -57,13 +76,26 @@ def build_faiss_index(check_point_dir: Path, BATCH_SIZE=512, STEP=800, PSGS_PATH
 
     loader = read_tsv(PSGS_PATH, BATCH_SIZE)
     with torch.no_grad():
-        for steps, batch in enumerate(tqdm(loader, desc="Encoding passages", unit="batch"),
-                                      start=0):
-            titles = [item["title"] for item in batch]
-            texts = [item["text"] for item in batch]
-            p_token = RiskMCLS_tokenizer()
+        for steps, batch in enumerate(tqdm(loader, desc="Encoding passages", unit="batch"), start=0):
+            embs = []
+            for item in batch:
+                p_token = tokenizer.p_tokenize(item["title"], item["text"])
+                # tensor all item
+                p_token = {
+                    "input_ids": torch.tensor(p_token["input_ids"], dtype=torch.long).unsqueeze(0).to("cuda"),
+                    "attention_mask": torch.tensor(p_token["attention_mask"], dtype=torch.long).unsqueeze(0).to("cuda"),
+                    "token_type_ids": torch.tensor(p_token["token_type_ids"], dtype=torch.long).unsqueeze(0).to("cuda"),
+                    "mcls_positions": p_token["mcls_positions"].to("cuda"),
+                    "mcls_mask": p_token["mcls_mask"].to("cuda"),
+                }
+                mcls, mcls_mask = model.encode_passage(**p_token)
 
-            emb = model.encode_passage(**p_token)
+                mcls_mask_ = mcls_mask.unsqueeze(-1).to(mcls.device)  # (1, M)               
+                denom = mcls_mask_.sum(dim=1).clamp(min=1)          # (1, 1)
+                pooled = (mcls * mcls_mask_).sum(dim=1) / denom     # (1, H)
+
+                embs.append(pooled)
+            emb = torch.cat(embs, dim=0)
             X = emb.cpu().numpy().astype(np.float32)
             I = np.asarray([item["id"] for item in batch], dtype=np.int64)
             index.add_with_ids(X, I)
