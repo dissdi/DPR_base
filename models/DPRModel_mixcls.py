e8886c7e44a4da78f7e3cf2977377c144dbfdc5c
Talusor <lth030326@naver.com>
2026-01-30 07:35:20 +0900

    improve mixcls model

--- models/DPRModel_mixcls.py
+++ models/DPRModel_mixcls.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,16 +9,16 @@ from transformers import AutoModel
 
 # LayerAttention -> ScalarMix
 class DPR_mixcls(nn.Module):
-    def __init__(self, loss_fn=None, tau = 0.07, q_mix_layer = 3, p_mix_layer = 6, projection = False):
+    def __init__(self, loss_fn=None, tau = 0.07, mix_layer = 8, projection = True):
         super().__init__()
         self.q_encoder = AutoModel.from_pretrained("bert-base-uncased", output_hidden_states=True)
         self.p_encoder = AutoModel.from_pretrained("bert-base-uncased", output_hidden_states=True)
 
-        self.layer_mix_q = ScalarMixHead(num_layers=q_mix_layer, proj=projection)
-        self.layer_mix_p = ScalarMixHead(num_layers=p_mix_layer, proj=projection)
-        
-        self.q_mix_layer = q_mix_layer
-        self.p_mix_layer = p_mix_layer
+        # q, p 를 나눠서 학습하면 공간 자체가 틀어질 가능성
+        self.layer_mix = ScalarMixHead(num_layers=mix_layer, proj=projection)
+
+        assert 1 < mix_layer <= 12, "mix_layer must be <= 12 and > 1"
+        self.mix_layer = mix_layer
 
         self.tau = tau
 
@@ -26,11 +28,11 @@ class DPR_mixcls(nn.Module):
         layers = self.q_encoder(input_ids=input_ids,
                                 attention_mask=attention_mask,
                                 token_type_ids=token_type_ids,
-                                return_dict=True).hidden_states[(13 - self.q_mix_layer):13]  # bert-base is 12 layer
+                                return_dict=True).hidden_states[(13 - self.mix_layer):13]  # bert-base is 12 layer
 
         cls_layers = [h[:, 0, :] for h in layers]
         
-        mixed_cls, _ = self.layer_mix_q(cls_layers)
+        mixed_cls, _ = self.layer_mix(cls_layers)
         
         return F.normalize(mixed_cls, dim=1)
 
@@ -39,10 +41,10 @@ class DPR_mixcls(nn.Module):
         layers = self.p_encoder(input_ids=input_ids,
                                 attention_mask=attention_mask,
                                 token_type_ids=token_type_ids,
-                                return_dict=True).hidden_states[(13 - self.p_mix_layer):13]  # bert-base is 12 layer
+                                return_dict=True).hidden_states[(13 - self.mix_layer):13]  # bert-base is 12 layer
 
         cls_layers = [h[:, 0, :] for h in layers]
-        mixed_cls, _ = self.layer_mix_p(cls_layers)
+        mixed_cls, _ = self.layer_mix(cls_layers)
         
         return F.normalize(mixed_cls, dim=1)
 
@@ -76,16 +78,31 @@ class DPR_mixcls(nn.Module):
 
 #
 class ScalarMixHead(nn.Module):
-    def __init__(self, hidden_size=768, num_layers=12, proj=True, dropout=0.1):
+    def __init__(self, hidden_size=768, num_layers=12, proj=True, dropout=0.1, init_weight=0.65):
         super().__init__()
-        self.w = nn.Parameter(torch.zeros(num_layers))  # softmax -> weights
+
+        # 각 레이어의 가중치를 결정하는 layer_w
+        self.layer_w = nn.Parameter(torch.zeros(num_layers))
+
+        # Dropout Layer
         self.dropout = nn.Dropout(dropout)
-        self.proj = nn.Linear(hidden_size, hidden_size) if proj else nn.Identity()
+
+        # Layer Norm
+        self.layer_norm = nn.LayerNorm(hidden_size)
+
+        # Projection Layer (가중합으로 합친 후 적절한 공간으로 다시 투영[정렬])
+        self.proj = nn.Sequential(nn.Linear(hidden_size, hidden_size), nn.GELU()) if proj else nn.Identity()
+
+        # 초기 layer_w를 랜덤하게 사용하면 학습이 불안정해 질 수 있으므로 마지막 레이어의 가중치를 init_weight으로 초기화
+        with torch.no_grad():
+            self.layer_w.data.fill_(0.0)
+            self.layer_w.data[-1] = math.log((init_weight * (num_layers - 1)) / (1.0 - init_weight))
 
     def forward(self, cls_layers):  # list of [B,H]
         X = torch.stack(cls_layers, dim=1)          # [B,L,H]
-        alpha = torch.softmax(self.w, dim=0)        # [L]
+        alpha = F.softmax(self.layer_w, dim=0)        # [L]
         z = (X * alpha.view(1, -1, 1)).sum(dim=1)   # [B,H]
         z = self.proj(z)
+        z = self.layer_norm(z)
         z = self.dropout(z)
         return z, alpha
